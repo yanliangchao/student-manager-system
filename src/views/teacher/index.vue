877fe38a7f200fc5c267b9b798132cb5fa0765dc
yanliangchao <liangchao.yan@outlook.com>
2024-06-02 19:26:06 +0800

    取消教师违纪！

--- src/views/teacher/index.vue
+++ src/views/teacher/index.vue
@@ -34,9 +34,9 @@
 				</el-table-column>
 				<el-table-column label="操作" width="150">
 					<template #default="scope">
-						<el-button :disabled="scope.row.username === 'admin'" size="small" text type="success" @click="onOpenDetails(scope.row)"
+						<!-- <el-button :disabled="scope.row.username === 'admin'" size="small" text type="success" @click="onOpenDetails(scope.row)"
 							>违纪</el-button
-						>
+						> -->
 						<el-button :disabled="scope.row.username === 'admin'" size="small" text type="primary" @click="onOpenEditUser('edit', scope.row)"
 							>修改</el-button
 						>
